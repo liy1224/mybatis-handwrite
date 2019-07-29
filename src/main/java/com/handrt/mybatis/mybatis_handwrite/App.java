01c63d5ef2fe24da4354a8c5f907b1120e7c59f9
liyangbj <liyang@vip.ubeii.com>
2019-07-29 16:25:15 +0800

    手动编写mybatis源码解析

---
+++ src/main/java/com/handrt/mybatis/mybatis_handwrite/App.java
@@ -0,0 +1,13 @@
+package com.handrt.mybatis.mybatis_handwrite;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
